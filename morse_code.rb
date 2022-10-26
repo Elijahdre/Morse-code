90ff973645e39a14504f525be303e01a26477c9c
Elijah Odjegba <elijahodjegba@gmail.com>
2022-10-26 15:28:13 +0100

    Decode morse_code

--- morse_code.rb
+++ morse_code.rb
@@ -12,5 +12,23 @@ MORSECODE = [
 def decode_char(char)
   ABC[MORSECODE.find_index(char)]
 end
+def decode_word(word)
+  separate = word.split(' ')
+  full_word = []
+  separate.each { |letter| full_word.append(decode_char(letter)) }
+  full_word.join
+end
+
+def decode(sentence)
+  separate = sentence.split('   ')
+  full_sentence = []
+
+  separate.each do |word|
+    full_sentence.append(decode_word(word))
+  end
+
+  full_sentence.join(' ')
+end
+
 
 p decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
